7377ea9833e8780efec53712eb15b2169ac04350
ydy <517697206@qq.com>
2024-06-14 17:02:28 +0800

    feat:test  for remote dashboard link

--- src/Caviar.AppHost/Program.cs
+++ src/Caviar.AppHost/Program.cs
@@ -7,8 +7,12 @@ using Caviar.AppHost;
 var builder = DistributedApplication.CreateBuilder(args);
 builder.AddForwardedHeaders();
 var launchProfileName = ShouldUseHttpForEndpoints() ? "http" : "https";
-
 var webApp = builder.AddProject<Projects.Caviar_AntDesignBlazor>("CaviarAntDesignBlazor",launchProfileName)
+     //The OTLP endpoint. This endpoint hosts an OTLP service and receives telemetry. When the dashboard is launched by the .NET Aspire app host this address is secured with HTTPS.
+     //Securing the dashboard with HTTPS is recommended.
+     // .WithEnvironment("DOTNET_DASHBOARD_URL", "http://192.168.99.100:18888")
+     // .WithEnvironment("OTEL_EXPORTER_OTLP_ENDPOINT", "http://192.168.99.100:18889")
+     ////.WithEnvironment("RESOURCE_SERVICE_ENDPOINT_URL", "http://192.168.99.100:18889")
     .WithExternalHttpEndpoints();
 builder.Build().Run();
 
